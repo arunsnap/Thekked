7b6f6d564bf574d665ebab077e3309954a9e7b34
arunsnap <106102148+arunsnap@users.noreply.github.com>
2026-08-18 23:37:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((error) => {
+        console.error("MongoDB connection error:", error);
+    });
 
 app.get("/", (req, res) => {
     res.json({
