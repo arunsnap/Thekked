86e130e10e00ea072ee931180d4128f512327052
arunsnap <106102148+arunsnap@users.noreply.github.com>
2026-08-19 00:39:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const productRoutes = require("./routes/products");
 
