ac1ea164a616e2f7fa8676aca9831a6e726fc9a4
arunsnap <106102148+arunsnap@users.noreply.github.com>
2026-08-19 00:41:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/products", productRoutes);
 
 mongoose.connect(process.env.MONGODB_URI)
